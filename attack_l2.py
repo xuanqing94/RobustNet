5d15ffc8e14ec5c9ee26e7e5fd068aef28494316
Minhao Cheng <cmhcbb@muradin.cs.ucdavis.edu>
2017-11-08 19:25:07 -0800

    fgsm added

--- attack_l2.py
+++ attack_l2.py
@@ -13,40 +13,34 @@ import numpy as np
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
-def attack_fgsm(input_v, label_v, net, loss, eps, TARGETED = False):
-    input_v.requires_grad = True
-    adverse = torch.FloatTensor(input_v.size()).zero_().cuda()
-    adverse_v = Variable(adverse, requires_grad= True)
-    outputs = net(input_v)
-    #softmax = nn.Softmax()
-    #outputs = softmax(outputs)
-    #print(outputs)
-    error = loss(outputs,label_v)
-    error.backward()
-    
-    grad = torch.sign(input_v.grad.data)
-    #adverse_v.data = torch.clamp(input_v.data + epsilon* grad, 0, 1)
-    adverse_v.data = input_v.data + eps * grad
-    return adverse_v
-
-def iter_attack_fgsm(input_v, label_v, net, loss, alpha, eps, TARGETED= False):
-    #input_v.requires_grad = True
-    adverse = input_v.data.clone()
-    adverse_v = Variable(adverse, requires_grad= True)
-    for _ in range(30):
-        outputs = net(adverse_v)
-        #softmax = nn.Softmax()
-        #outputs = softmax(outputs)
-        #print(outputs)
-        error = loss(outputs,label_v)
+def attack(input_v, label_v, net, c, TARGETED=False):
+    n_class = len(classes)
+    index = label_v.data.cpu().view(-1,1)
+    label_onehot = torch.FloatTensor(input_v.size()[0] , n_class)
+    label_onehot.zero_()
+    label_onehot.scatter_(1,index,1)
+    label_onehot_v = Variable(label_onehot, requires_grad = False).cuda()
+	#print(label_onehot.scatter)
+    adverse = input_v.data.clone() #torch.FloatTensor(input_v.size()).zero_().cuda()
+    adverse_v = Variable(adverse, requires_grad=True)
+    optimizer = optim.Adam([adverse_v], lr=0.01)
+    for _ in range(300):
+        optimizer.zero_grad()
+        diff = adverse_v - input_v
+        output = net(adverse_v)
+        #print(output.size())
+        real = torch.max(torch.mul(output, label_onehot_v), 1)[0]
+        other = torch.max(torch.mul(output, (1-label_onehot_v))-label_onehot_v*10000,1)[0]
+        #print(real,other)
+        error = torch.sum(diff * diff)
+        #print(error.size())
+        if TARGETED:
+            error += c * torch.sum(other - real)
+        else:
+            error += c * torch.sum(real - other)
         error.backward()
-        normed_grad = alpha * torch.sign(adverse_v.grad.data)
-        step_adv = adverse_v.data + normed_grad
-        adv = torch.clamp(step_adv - input_v.data, -eps , eps)
-        result = torch.clamp(input_v.data + adv, 0, 1)
-        adverse_v.data = result
-    return adverse_v 
+        optimizer.step()
+    return adverse_v
 
 def weights_init(m):
     classname = m.__class__.__name__
@@ -62,8 +56,7 @@ def weights_init(m):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--modelIn', type=str, default=None)
-    parser.add_argument('--alpha', type=float, default=1.0)
-    parser.add_argument('--eps', type=float, default=1.0)
+    parser.add_argument('--c', type=float, default=1.0)
     parser.add_argument('--noise', type=float, default=0)
     opt = parser.parse_args()
 
@@ -95,7 +88,7 @@ if __name__ == "__main__":
     count, count2 = 0, 0
     for input, output in dataloader_test:
         input_v, label_v = Variable(input.cuda()), Variable(output.cuda())
-        adverse_v = attack_fgsm(input_v, label_v, net, loss_f, eps=opt.eps)
+        adverse_v = attack(input_v, label_v, net, opt.c)
         _, idx = torch.max(net(input_v), 1)
         _, idx2 = torch.max(net(adverse_v), 1)
         count += torch.sum(label_v.eq(idx)).data[0]
