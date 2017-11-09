5a3e968ff8013dc16697e8be22950b9e98f027dc
Xuanqing Liu <xqliu@raynor.ucdavis.edu>
2017-11-08 19:52:37 -0800

    add BReLU, Normalization, DeNormalization

--- run_attack.sh
+++ run_attack.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-CUDA_VISIBLE_DEVICES=3 ./attack.py --root ~/data/cifar10-py --modelIn ./vgg16/noise_0.05_0.1.pth --noiseInit 0. --noiseInner 0. --c 0.5
+CUDA_VISIBLE_DEVICES=3 ./attack.py --root ~/data/cifar10-py --modelIn ./vgg16/noise_0_0.pth --noiseInit 0 --noiseInner 0 --c 0.1
