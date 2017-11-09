842a398cbc772d4273eda9e66c7363bead23ea67
Xuanqing Liu <xqliu@raynor.ucdavis.edu>
2017-11-08 19:52:37 -0800

    add BReLU, Normalization, DeNormalization

--- models/layer.py
+++ models/layer.py
@@ -17,3 +17,33 @@ class Noise(nn.Module):
             return x + self.buffer
         return x
 
+class BReLU(nn.Module):
+    def __init__(self, t):
+        super(BReLU, self).__init__()
+        assert(t > 0)
+        self.t = t
+
+    def forward(self, x):
+        return x.clamp(0, self.t)
+
+class Normalize(nn.Module):
+    def __init__(self, mean_vec, std_vec):
+        super(Normalize, self).__init__()
+        self.mean = Variable(mean_vec.view(1, 3, 1, 1), requires_grad=False)
+        self.std = Variable(std_vec.view(1, 3, 1, 1), requires_grad=False)
+
+    def forward(self, x):
+        # x: (batch, 3, H, W)
+        # mean, std: (1, 3, 1, 1)
+        return (x - self.mean) / self.std
+        #return x
+
+class DeNormalize(nn.Module):
+    def __init__(self, mean_vec, std_vec):
+        super(DeNormalize, self).__init__()
+        self.mean = Variable(mean_vec.view(1, 3, 1, 1), requires_grad=False)
+        self.std = Variable(std_vec.view(1, 3, 1, 1), requires_grad=False)
+
+    def forward(self, x):
+        return x * self.std + self.mean
+        #return x
